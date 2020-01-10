0e5ded14b71e584e7f83703b2c0a3c4c4613a8a9
Kasper Seweryn <github@wvffle.net>
2020-01-10 18:25:02 +0100

    Fix all stuff and make window showup
    Signed-off-by: Kasper Seweryn <github@wvffle.net>

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,20 @@ use gdk::*;
 
 const GTK_STYLE_PROVIDER_PRIORITY_USER: i32 = 800;
 
+
 fn main() {
+    const COL_PADDING: u32 = 17;
+
+    let config_wal = 0;
+    let config_columns = 4;
+    let config_prompt = "run:";
+
     open_config();
     let desktop_entries = find_desktop_entries();
 
     gtk::init().expect("Could not init GTK!");
 
-    let window = Window::new(WindowType::Toplevel);
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
     init_for_window(&window);
     set_layer(&window, Layer::Top);
     set_anchor(&window, Edge::Top, true);
@@ -29,7 +36,7 @@ fn main() {
 
     let grid_width = 270 * config_columns + (config_columns + 2) * COL_PADDING;
     let spacer = Box::new(Orientation::Horizontal, 0);
-    let width = get_monitor_width() - grid_width / 2;
+    let width = get_monitor_width() - (grid_width as i32) / 2;
     spacer.set_size_request(width, 1);
 
     search_box.pack_start(&spacer, false, false, 0);
@@ -38,22 +45,22 @@ fn main() {
     search_label.set_markup(config_prompt);
     search_box.pack_start(&search_label, false, false, 0);
 
-    search_label.add_class("textview-label");
+    add_class(&search_label, "textview-label");
 
     let search_input = TextView::new();
-    search_input.set_name("search");
+//    search_input.set_name("search");
     search_box.pack_start(&search_input, true, true, 0);
 
     let app_grid = Grid::new();
     layout.pack_start(&app_grid, true, true, 0);
 
     for i in 0..config_columns {
-        app_grid.insert_column(i);
+        app_grid.insert_column(i as i32);
     }
 
     app_grid.set_column_spacing(COL_PADDING);
     app_grid.set_row_spacing(COL_PADDING);
-    app_grid.set_name("apps");
+//    app_grid.set_name("apps");
 
     app_grid.set_halign(Align::Center);
 
@@ -62,13 +69,17 @@ fn main() {
     let css_provider = CssProvider::new();
 
     if config_wal == 1 {
-        window.add_class("pywal");
+        add_class(&window, "pywal");
     }
 
     let css = get_css();
 
     if let Ok(_) = css_provider.load_from_data(css.as_ref()) {
-        css_provider.add_provider_for_screen(&window, css, GTK_STYLE_PROVIDER_PRIORITY_USER);
+        StyleContext::add_provider_for_screen(
+            &gdk::Screen::get_default().expect("Error initializing css provider"),
+            &css_provider,
+            STYLE_PROVIDER_PRIORITY_USER,
+        );
     }
 
     if let Some(display) = window.get_display() {
@@ -90,7 +101,7 @@ struct DesktopEntry {
 }
 
 fn get_css() -> String {
-
+    return String::from("window { background: alpha(#000, .7) }");
 }
 
 fn update_apps(apps: &Vec<DesktopEntry>) {
@@ -98,13 +109,18 @@ fn update_apps(apps: &Vec<DesktopEntry>) {
 }
 
 fn get_monitor_width() -> i32 {
-
+    return 1920;
 }
 
 fn find_desktop_entries() -> Vec<DesktopEntry> {
-
+    return Vec::<DesktopEntry>::new();
 }
 
 fn open_config() {
 
+}
+
+fn add_class<W: gtk::prelude::WidgetExt>(widget: &W, class_name: &str) {
+    let ctx = widget.get_style_context();
+    ctx.add_class(class_name);
 }
